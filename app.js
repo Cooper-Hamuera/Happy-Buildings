c977971afaab1177eedfb1023eb7eff0bcd37c29
Lewis <lewis.edwards.ep@gmail.com>
2017-04-12 13:35:34 +1200

    Updated Test with feedback

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 /**
- * Created by Glamdring on 12/04/17.
+ * Created by Lewis on 12/04/17.
  */
-var app = angular.module('plunker', []);
+var app = angular.module('HappyB', []);
 
+//sets feedback check to null
 app.controller('MainCtrl', function($scope) {
-    $scope.name = 'World';
+    $scope.feedback = "";
+
+    //check user input against password
+    $scope.checkLogin=function(){
+        var usernameAccess="admin";
+        var passwordAccess="password";
+
+        if($scope.if_username==usernameAccess & $scope.if_password==passwordAccess){
+            $scope.feedback="login successful";
+        }else{
+            $scope.feedback="Username or Password incorrect";
+        }
+    };
+
+    //cancel button clears the username and password fields
+    $scope.cancelLogin=function(){
+        $scope.if_username="";
+        $scope.if_password="";
+        $scope.if.feedback="";
+    };
+
 });
