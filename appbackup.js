1ffd3585d79b7d3bf857e09b5181bfa77f554304
Lewis <lewis.edwards.ep@gmail.com>
2017-05-13 19:39:32 +1200

    Update to JSON
    Server communication with JSON, Local JSON, Show+Hide integration. Removed old 2.0 files, now using index.html

--- appbackup.js
+++ appbackup.js
@@ -1,6 +1,6 @@
 var app = angular.module('HappyB', []);
 
-//Navagation bar tabs
+//[JS] Navagation bar tabs
 function openTab(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -17,32 +17,80 @@ function openTab(evt, cityName) {
 //Navagation bar tabs
 
 
-//Controller
-app.controller('MainCtrl', function($scope) {
-    $scope.feedback = "";
-    $scope.loginVisible = true;
-    $scope.directoryVisible = false;
+//[Angular] Controller
+app.controller('MainCtrl', function($scope, $http) {
+    $scope.loginVisible = true;         //login visibility (Show/Hide)
+    $scope.indexVisible = false;        //index visibility (Show/Hide)
+    $scope.feedback = "";               //Stores feedback String
+    $scope.username = "";               //Username field
+    $scope.password = "";               //Password field
+    $scope.usernameValid = false;       //Username validation
+    $scope.passwordValid = false;       //Password validation
 
-    //check user input against credentials listed
-    $scope.checkLogin=function(){
-        var usernameAccess="admin";
-        var passwordAccess="password";
 
-        if($scope.if_username==usernameAccess & $scope.if_password==passwordAccess){
-            $scope.feedback="Login Successful";
-            $scope.loginVisible = false;
-            $scope.directoryVisible = true;
-        }else{
-            $scope.feedback="Username or Password Incorrect";
+    //JSON login
+    $scope.loginData = null;
+    $scope.loginLocation = 'user_list.json';    //Local JSON file *requires HTML header source
+
+    /**
+     Server URL's
+
+     $scope.loginLocation = 'https://happybuildings.sim.vuw.ac.nz/api/edwardlewi/user_list.json';
+     $scope.loginLocation = 'https://happybuildings.sim.vuw.ac.nz/api/coopersamu/user_list.json';
+
+     $scope.buildingDir = https://happybuildings.sim.vuw.ac.nz/api/edwardlewi/building_dir.json;
+     $scope.buildingDir = https://happybuildings.sim.vuw.ac.nz/api/coopersamu/building_dir.json;
+
+     $scope.buildingDir = https://happybuildings.sim.vuw.ac.nz/api/edwardlewi/building_dir.json;
+     $scope.buildingDir = https://happybuildings.sim.vuw.ac.nz/api/coopersamu/building_dir.json;
+
+     $scope.buildingDir = https://happybuildings.sim.vuw.ac.nz/api/edwardlewi/building_dir.json;
+     $scope.buildingDir = https://happybuildings.sim.vuw.ac.nz/api/coopersamu/building_dir.json;
+
+     $scope.buildingDir = https://happybuildings.sim.vuw.ac.nz/api/edwardlewi/building_dir.json;
+     $scope.buildingDir = https://happybuildings.sim.vuw.ac.nz/api/coopersamu/building_dir.json;
+     **/
+
+    $scope.get = $http.get($scope.loginLocation)      //Fetch the JSON file
+        .then(
+            function successCall(response) {
+                $scope.loginData = response.data.users; //Saves json response into variable
+            }, function errorCall(response) {
+                $scope.feedback = "Error reading file: " + response.status;
+                $scope.loginData = null; // if there is an error your data wont be populated
+            }
+        );
+
+    $scope.checkLogin = function() {
+        if ($scope.loginData === null) {
+            $scope.feedback = "Sorry, error reading file";
+        }
+        for (i = 0; $scope.loginData !== null && i < $scope.loginData.length; i++) { //reads one element at a time
+            if ($scope.loginData[i].LoginName == $scope.username) { //if that element, equals what the user has inputed
+                $scope.usernameValid = true; //then the username is valid
+            }
+            if ($scope.loginData[i].Password == $scope.password) { //if that element equals the password the user has inputted
+                $scope.passwordValid = true; // then the password is valid
+            }
+        }
+
+        //check login data functions
+        if ($scope.usernameValid && $scope.passwordValid) { //if both the username and password are contained in the server
+            $scope.feedback = "Login successful as " + $scope.username; //your login will be successful and..
+            $scope.loginVisible = false; //login form will then be hidden and..
+            $scope.indexVisible = true; // the directory will be displayed
+        } else {
+            $scope.feedback = "Login failed";
         }
     };
 
+
     //cancel button clears the username and password fields
     $scope.cancelLogin=function(){
-        $scope.if_username="";
-        $scope.if_password="";
-        $scope.if.feedback="";
+        $scope.username="";
+        $scope.password="";
+        $scope.feedback="";
     };
 
 });
-//Login feedback
+//[Angular] Controller End
